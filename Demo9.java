570c894efe9e78a458afb6dd3004ca3f95ef9e15
Shourya1224 <shourya21m@gmail.com>
2025-05-05 21:52:06 +0530

    hello

---
+++ Demo9.java
@@ -0,0 +1,37 @@
+public class Demo9 {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    
+    public double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: Cannot divide by zero.");
+            return Double.NaN;
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Demo9 calc = new Demo9 ();  
+
+        double a = 10;
+        double b = 5;
+
+        System.out.println("Addition: " + calc.add(a, b));
+        System.out.println("Subtraction: " + calc.subtract(a, b));
+        System.out.println("Multiplication: " + calc.multiply(a, b));
+        System.out.println("Division: " + calc.divide(a, b));
+    }
+}
+    
+
